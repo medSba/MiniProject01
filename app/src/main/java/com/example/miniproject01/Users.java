a458423396e1557ec2b3bfe60c49448afc353419
Ahmed Essebaiy <ahmadessebaiy@gmail.com>
2023-06-12 22:34:38 +0100

    Display users in custom ListView - create Users Model class

---
+++ app/src/main/java/com/example/miniproject01/Users.java
@@ -0,0 +1,50 @@
+package com.example.miniproject01;
+
+public class Users {
+    private String firstName;
+    private String lastName;
+    private String gender;
+    private String city;
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public Users(String firstName, String lastName, String gender, String city) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.gender = gender;
+        this.city = city;
+    }
+
+    public String fullName(){
+        return String.format("%s %s",firstName,lastName);
+    }
+}

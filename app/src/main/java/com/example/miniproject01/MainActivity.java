7d3d8f075f47fe5ef3a9c79c3d6d2154491e06e0
Ahmed Essebaiy <ahmadessebaiy@gmail.com>
2023-06-12 22:21:29 +0100

    Display users in simple ListView

--- app/src/main/java/com/example/miniproject01/MainActivity.java
+++ app/src/main/java/com/example/miniproject01/MainActivity.java
@@ -5,7 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import org.json.JSONArray;
@@ -14,9 +16,11 @@ import org.json.JSONObject;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     Button btnquit,btnusers;
+    ListView lvUsers;
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -26,6 +30,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         btnquit=findViewById(R.id.btnquit);
         btnusers=findViewById(R.id.btnusers);
+        lvUsers=findViewById(R.id.lvUsers);
 
         btnusers.setOnClickListener(this);
         btnquit.setOnClickListener(this);
@@ -35,32 +40,39 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
         if (v.getId()==R.id.btnusers){
-            try {
-                InputStream inputStream=getAssets().open("users.json");
-                int code;
-                StringBuilder sb=new StringBuilder();
-                String jsonString;
-                code=inputStream.read();
-                while (code!=-1){
-                    sb.append((char)code);
-                    code=inputStream.read();
-                }
-                jsonString=sb.toString();
-                JSONObject jsonObject=new JSONObject(jsonString);
-                JSONArray jsonArray=jsonObject.getJSONArray("users");
-                StringBuilder sbfname=new StringBuilder();
-                for (int i = 0; i < jsonArray.length(); i++) {
-                    JSONObject user=jsonArray.getJSONObject(i);
-                    JSONObject userName=user.getJSONObject("name");
-                    String fullname=String.format("%s $s\n",userName.get("first"),userName.get("last"));
-                    sbfname.append(fullname);
-                }
-                Toast.makeText(this, sbfname, Toast.LENGTH_SHORT).show();
-            } catch (IOException |JSONException e) {
-                throw new RuntimeException(e);
-            }
+            ArrayAdapter<String> adapter=new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,getUsers());
+            getUsers();
+            lvUsers.setAdapter(adapter);
         }else if (v.getId()==R.id.btnquit){
             finish();
         }
+    }
+
+    private ArrayList<String> getUsers() {
+        ArrayList<String> Users=new ArrayList<>();
+        try {
+            InputStream inputStream=getAssets().open("users.json");
+            int code;
+            StringBuilder sb=new StringBuilder();
+            String jsonString;
+            code=inputStream.read();
+            while (code!=-1){
+                sb.append((char)code);
+                code=inputStream.read();
+            }
+            jsonString=sb.toString();
+            JSONObject jsonObject=new JSONObject(jsonString);
+            JSONArray jsonArray=jsonObject.getJSONArray("users");
+
+            for (int i = 0; i < jsonArray.length(); i++) {
+                JSONObject user=jsonArray.getJSONObject(i);
+                JSONObject userName=user.getJSONObject("name");
+                String fullname=String.format("%s %s\n",userName.get("first"),userName.get("last"));
+                Users.add(fullname);
+            }
+        } catch (IOException |JSONException e) {
+            throw new RuntimeException(e);
+        }
+        return Users;
     }
 }

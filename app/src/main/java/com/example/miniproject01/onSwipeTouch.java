6218dbb8a3691d0c6274da8a06914cbe2277e3d1
Ahmed Essebaiy <ahmadessebaiy@gmail.com>
2023-06-13 23:05:03 +0100

    Convert quit Button to TextView: implement Swipe left event - Refactoring V2

--- app/src/main/java/com/example/miniproject01/onSwipeTouch.java
+++ app/src/main/java/com/example/miniproject01/onSwipeTouch.java
@@ -23,11 +23,18 @@ class GestureListenner extends GestureDetector.SimpleOnGestureListener{
         public boolean onFling(@NonNull MotionEvent e1, @NonNull MotionEvent e2, float velocityX, float velocityY) {
             if (e1.getX()-e2.getX()>THRESHOLD)
                 swipeLeft();
+            else if (e2.getX()-e1.getX()>THRESHOLD)
+                swipeRight();
             return super.onFling(e1, e2, velocityX, velocityY);
         }
     }
 
-    protected abstract void swipeLeft();
+    protected void swipeLeft(){
+
+    }
+    protected void swipeRight(){
+
+    }
 
     @Override
     public boolean onTouch(View v, MotionEvent event) {

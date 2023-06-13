7eb8b1e1342b95ca72b096ffe8004f20d00d623c
Ahmed Essebaiy <ahmadessebaiy@gmail.com>
2023-06-13 19:05:13 +0100

    Add setOnTouchListener to User Item implements DoubleClick

--- app/src/main/java/com/example/miniproject01/UserAdapter.java
+++ app/src/main/java/com/example/miniproject01/UserAdapter.java
@@ -2,12 +2,15 @@ package com.example.miniproject01;
 
 import android.content.Context;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -16,6 +19,7 @@ import androidx.appcompat.app.AlertDialog;
 import java.util.ArrayList;
 
 public class UserAdapter extends BaseAdapter {
+    final int DOUBLE_CLICK=250;
     Context context;
     private ArrayList<Users> user;
     LayoutInflater inflater;
@@ -48,16 +52,39 @@ public class UserAdapter extends BaseAdapter {
         convertView=inflater.inflate(R.layout.item_user,null);
         tvfname=convertView.findViewById(R.id.tvitemFullname);
         tvcity=convertView.findViewById(R.id.tvitemCity);
+        ImageView ivcheck=convertView.findViewById(R.id.imgvchecked);
 
         tvfname.setText(users.fullName());
         tvcity.setText(users.getCity());
-       convertView.setOnLongClickListener(new View.OnLongClickListener() {
+//       convertView.setOnLongClickListener(new View.OnLongClickListener() {
+//           @Override
+//           public boolean onLongClick(View v) {
+//               AlertDialog.Builder builder=new AlertDialog.Builder(context);
+//               builder.setTitle("Details of user "+(position+1));
+//               builder.setMessage(users.toString()).show();
+//               return false;
+//           }
+//       });
+       convertView.setOnTouchListener(new View.OnTouchListener() {
+           long lastclick=0;
            @Override
-           public boolean onLongClick(View v) {
-               AlertDialog.Builder builder=new AlertDialog.Builder(context);
-               builder.setTitle("Details of user "+(position+1));
-               builder.setMessage(users.toString()).show();
-               return false;
+           public boolean onTouch(View v, MotionEvent event) {
+               switch (event.getAction()){
+//                   case MotionEvent.ACTION_DOWN:
+//                       Toast.makeText(context, "Down", Toast.LENGTH_SHORT).show();
+//                       break;
+                   case MotionEvent.ACTION_UP:
+                       long clicktime=System.currentTimeMillis();
+                       if ((clicktime-lastclick) <= DOUBLE_CLICK){
+                           ivcheck.setVisibility(ivcheck.getVisibility()==View.INVISIBLE ? View.VISIBLE: View.INVISIBLE);
+                       }
+                       else {
+                           lastclick=clicktime;
+                       }
+                       break;
+               }
+
+               return true;
            }
        });
 
